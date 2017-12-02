64801b03d28bed0a89a63c6cc5a531345340e459
Diljot Garcha <diljiboy@gmail.com>
2017-12-02 11:12:58 -0600

    Stuff

--- Parser.py
+++ Parser.py
@@ -1,2 +1,11 @@
-import requests
-from bs4 import BeautifulSoup
+from linkedin import linkedin
+
+API_KEY = ''
+API_SECRET = ''
+RETURN_URL = 'https://diljotsg.com/'
+
+authentication = linkedin.LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL)
+print authentication.authorization_url  # open this url on your browser
+application = linkedin.LinkedInApplication(authentication)
+
+application.get_profile()
